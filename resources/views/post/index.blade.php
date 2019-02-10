7726b05b1a33b5828f81d2ea4d2b8a74cd3fcc65
Chinmay Gangal <chinmay2312@gmail.com>
2019-02-10 13:33:13 -0600

    - laravel working - navbar header in place

---
+++ resources/views/post/index.blade.php
@@ -0,0 +1,17 @@
+
+{{--
+ - Created by PhpStorm.
+ - User: chinm
+ - Date: 10-02-2019
+ - Time: 12:55
+ --}}
+
+@extends('layouts.master')
+
+@section('title')
+    Amazing Blog
+@endsection
+
+@section('content')
+    <h1>Welcome to Chinmay's Laravel blog</h1>
+@endsection
